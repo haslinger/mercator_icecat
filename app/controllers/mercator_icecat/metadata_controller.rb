3def059a57fdd4ba12e7b3b595aca92de03c9b93
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-04-14 08:46:11 +0200

    model controller namespace

--- app/controllers/mercator_icecat/metadata_controller.rb
+++ app/controllers/mercator_icecat/metadata_controller.rb
@@ -1,7 +1,7 @@
 module MercatorIcecat
   class MetadataController < ApplicationController
 
-    hobo_model_controller MercatorIcecat::Metadatum
+    hobo_model_controller "MercatorIcecat::Metadatum"
     auto_actions :all
   end
 end
